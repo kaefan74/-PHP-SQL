4edd7658d8020336def24c174b108931f2c8f5dc
kae_fan <62436375+kaefan74@users.noreply.github.com>
2020-05-18 15:30:40 +0900

    commit

---
+++ mini_bbs/dbconnect.php
@@ -0,0 +1,16 @@
+<?php
+try{
+  $db = new PDO('mysql:dbname=mini_bbs;host=localhost;port=8889;
+  charset=utf8','root','root');
+} catch(PDOException $e) {
+  print('DB接続エラー:'.$e->getMessage());
+}
+?>
+<?php
+// try{
+//   $db = new PDO('mysql:dbname=mini_bbs;host=localhost;port=8889;
+//   charset=utf8','root','root');
+// }catch(PDOException $e){
+//   print('DB接続エラー:'. $e->getMessage());
+// }
+?>
